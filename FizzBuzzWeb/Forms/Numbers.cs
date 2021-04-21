42da11696b3322dcd0fd24123f5928ed506d714b
SOnichimiuk <82914282+SOnichimiuk@users.noreply.github.com>
2021-04-21 02:24:16 +0200

    Add files via upload

---
+++ FizzBuzzWeb/Forms/Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb.Forms
+{
+    public class Numbers
+    {
+        [Display(Name = "Liczba:")]
+        [Required(ErrorMessage = "Podaj liczbę!")]
+        [Range(1, 1000, ErrorMessage = "Podaj liczbę z zakresu 1-1000!")]
+        public int Number { get; set; }
+
+        public string Date { get; set; }
+
+        public string Message { get; set; }
+    }
+}
